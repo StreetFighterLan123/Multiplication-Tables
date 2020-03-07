f4c8a3edc72c21253a8113a5e38e6571a981be6e
Omar Ansari <pi@raspberrypi>
2020-03-07 16:47:26 -0600

    Made the first digits list. Probably not using "1_tables".

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import random
 
 
 
-#for i in range(1,13):
-	#if i == 1:
+
+	
 		
 
 
@@ -17,6 +17,9 @@ import random
 def main():
 	print "This project is still in development"
 	#Work on the main loop here.
+	first_digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+	#for i in range (1,13):
+		
 	
 	
 
